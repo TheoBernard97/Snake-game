08c445fb871b29e0392385f657752c1a1ab59685
Théo Bernard <theo.bernard.94100@gmail.com>
2020-06-15 23:06:49 +0200

    Install mongoose and start the backend server

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
 
-app.get("/", function (req, res) {
-  res.send("Hello World!");
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+
+const scoreSchema = new mongoose.Schema({
+  user: String,
+  score: Number,
+  rank: Number,
 });
 
-app.listen(5000, function () {
-  console.log("Example app listening on port 5000!");
-});
+const Score = mongoose.model("Score", scoreSchema);
+
+// const score = new Score({
+//   user: "User1",
+//   score: 5,
+//   rank: 1,
+// });
+
+// score.save();
